2adf31d008a4adc815ef4e8ff200e3db269167f8
kunze <tarek.kunze@ens.psl.eu>
2024-10-17 13:22:32 +0200

    chore: simplified receptive field function call

--- src/segma/utils/receptive_fields.py
+++ src/segma/utils/receptive_fields.py
@@ -1,7 +1,11 @@
 from math import prod
 
 
-def rf_start_i(u_L: int, strides: list[int], paddings: list[int], L: int) -> int:
+def rf_start_i(
+    u_L: int,
+    strides: list[int] | tuple[int, ...],
+    paddings: list[int] | tuple[int, ...],
+) -> int:
     """Computes the start index of the receptive field.
 
     see eq (5) in https://distill.pub/2019/computing-receptive-fields/
@@ -15,6 +19,8 @@ def rf_start_i(u_L: int, strides: list[int], paddings: list[int], L: int) -> int
     Returns:
         int: Start index of the receptive field in the input vector. Can be negative.
     """
+    L = len(strides)
+    assert L == len(paddings)
     S_0 = prod(strides)
 
     P_0 = 0
@@ -25,7 +31,10 @@ def rf_start_i(u_L: int, strides: list[int], paddings: list[int], L: int) -> int
 
 
 def rf_end_i(
-    v_L: int, kernels: list[int], strides: list[int], paddings: list[int], L: int
+    v_L: int,
+    kernels: list[int] | tuple[int, ...],
+    strides: list[int] | tuple[int, ...],
+    paddings: list[int] | tuple[int, ...],
 ) -> int:
     """Computes the end index of the receptive field.
 
@@ -36,11 +45,13 @@ def rf_end_i(
         kernels (list[int]): list of kernel sizes, from first to last convolutionnal layer.
         strides (list[int]): list of stride sizes, from first to last convolutionnal layer.
         paddings (list[int]): list of padding values (size of the right and left padding), from first to last convolutionnal layer.
-        L (int): number of layers to take into account.
 
     Returns:
         int: End index of the receptive field in the input vector. Can be greater than the size of the input vector.
     """
+    L = len(kernels)
+    assert L == len(strides) == len(paddings)
+
     S_0 = prod(strides)
 
     rt = 0
@@ -50,7 +61,10 @@ def rf_end_i(
     return v_L * S_0 - rt
 
 
-def rf_size(kernels: list[int], strides: list[int], L: int) -> int:
+def rf_size(
+    kernels: list[int] | tuple[int, ...],
+    strides: list[int] | tuple[int, ...],
+) -> int:
     """Computes the size of the receptive field.
 
     see eq (2) in https://distill.pub/2019/computing-receptive-fields/
@@ -58,13 +72,32 @@ def rf_size(kernels: list[int], strides: list[int], L: int) -> int:
     Args:
         kernels (list[int]): list of kernel sizes, from first to last convolutionnal layer.
         strides (list[int]): list of stride sizes, from first to last convolutionnal layer.
-        L (int): number of layers to take into account.
 
     Returns:
         int: Size of the receptive field.
     """
+    L = len(kernels)
+    assert L == len(strides)
 
     rf = 0
     for layer_i in range(L):
         rf += (kernels[layer_i] - 1) * prod(strides[:layer_i])
     return rf + 1
+
+
+def rf_center_i(
+    u_L: int,
+    kernels: list[int] | tuple[int, ...],
+    strides: list[int] | tuple[int, ...],
+    paddings: list[int] | tuple[int, ...],
+):
+    """Center of receptive field"""
+    L = len(kernels)
+    assert L == len(strides) == len(paddings)
+
+    S_0 = prod(strides)
+    P_0 = 0
+    for layer_i in range(L):
+        P_0 += paddings[layer_i] * prod(strides[:layer_i])
+
+    return u_L * S_0 + (rf_size(kernels, strides) - 1) / 2 - P_0
--- tests/test_receptive_fields.py
+++ tests/test_receptive_fields.py
@@ -6,9 +6,9 @@ def test_rf_start_i() -> None:
     paddings = [1, 0]
     strides = [3, 1]
 
-    assert rf_start_i(0, strides=strides, paddings=paddings, L=2) == -1
+    assert rf_start_i(0, strides=strides, paddings=paddings) == -1
     # Test for the receptive field at the 2nd layer
-    assert rf_start_i(0, strides=strides[1:], paddings=paddings[1:], L=1) == 0
+    assert rf_start_i(0, strides=strides[1:], paddings=paddings[1:]) == 0
 
 
 def test_rf_end_i() -> None:
@@ -16,12 +16,10 @@ def test_rf_end_i() -> None:
     paddings = [1, 0]
     strides = [3, 1]
 
-    assert rf_end_i(0, kernels=kernels, strides=strides, paddings=paddings, L=2) == 4
+    assert rf_end_i(0, kernels=kernels, strides=strides, paddings=paddings) == 4
     # Test for the receptive field at the 2nd layer
     assert (
-        rf_end_i(
-            0, kernels=kernels[1:], strides=strides[1:], paddings=paddings[1:], L=1
-        )
+        rf_end_i(0, kernels=kernels[1:], strides=strides[1:], paddings=paddings[1:])
         == 1
     )
 
@@ -31,6 +29,6 @@ def test_rf_size() -> None:
     _paddings = [1, 0]
     strides = [3, 1]
 
-    assert rf_size(kernels=kernels, strides=strides, L=2) == 6
+    assert rf_size(kernels=kernels, strides=strides) == 6
     # Test for the receptive field at the 2nd layer
-    assert rf_size(kernels=kernels[1:], strides=strides[1:], L=1) == 2
+    assert rf_size(kernels=kernels[1:], strides=strides[1:]) == 2

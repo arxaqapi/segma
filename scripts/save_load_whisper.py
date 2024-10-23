aca5ae34c9d92c917e7d789b1ce16c44ae71352f
kunze <tarek.kunze@ens.psl.eu>
2024-10-23 18:42:49 +0200

    chore fmt

--- scripts/save_load_whisper.py
+++ scripts/save_load_whisper.py
@@ -1,6 +1,5 @@
 from transformers import WhisperModel
 
-
 if __name__ == "__main__":
     model = WhisperModel.from_pretrained("openai/whisper-tiny")
     model.encoder.save_pretrained("whisper_tiny_encoder")

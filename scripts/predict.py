731b3c8b52cc549514ddcb6c5bce79e834220ed3
kunze <tarek.kunze@ens.psl.eu>
2024-11-28 15:42:05 +0100

    feat: predict is done on a folder of wav files

--- scripts/predict.py
+++ scripts/predict.py
@@ -1,6 +1,7 @@
+import argparse
 from pathlib import Path
 
-from segma.models import Minisinc  # , Whisperidou
+from segma.models import PyanNet
 from segma.predict import prediction
 from segma.utils.encoders import PowersetMultiLabelEncoder
 
@@ -9,5 +10,15 @@ if __name__ == "__main__":
         ["male", "female", "key_child", "other_child"]
     )
 
-    prediction(Path("data/debug/wav/0000.wav"), model=Minisinc(l_encoder))
-    # evaluate(Path("data/debug/wav/0000.wav"), model=Whisperidou(l_encoder))
+    # TODO - argparser to load a folder of wav
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--wavs", default="data/debug/wav")
+
+    args = parser.parse_args()
+
+    args.wavs = Path(args.wavs)
+
+    assert args.wavs.exists()
+
+    for wav_f in args.wavs.glob("*.wav"):
+        prediction(wav_f, model=PyanNet(l_encoder))
--- src/segma/predict.py
+++ src/segma/predict.py
@@ -5,20 +5,20 @@ import torch
 import torchaudio
 
 from segma.annotation import AudioAnnotation
-from segma.models import BaseSegmentationModel, Whisperidou
+from segma.models.base import BaseSegmentationModel
 from segma.structs.interval import Interval, Intervals
 from segma.utils.conversions import frames_to_milliseconds
 from segma.utils.receptive_fields import rf_end_i, rf_start_i
 
 
 def prediction(
-    audio_path: Path, model: BaseSegmentationModel, output_p: Path = Path("out")
+    audio_path: Path, model: BaseSegmentationModel, output_p: Path = Path("segma_out")
 ):
     """takes a path to an audio file and a trained model,
-    perform frame-level inference and stitch everything together
+    perform frame-level inference and stitch everything together.
+
+    Saves everything to an output file
     """
-    if not output_p.exists():
-        output_p.mkdir(exist_ok=True)
     assert audio_path.exists()
 
     audio_t, _sr = torchaudio.load(audio_path.resolve())
@@ -30,25 +30,22 @@ def prediction(
     audio_t = audio_t.squeeze()
 
     # create chunks of size chunk_size by padding and reshaping the audio
-    n_frames = len(audio_t)
-    chunck_size_f = 32_000
+    n_valid_frames = len(audio_t)
+    chunck_size_f = 2 * 16_000  # _sr
 
     # if < 1 pad pad to match batch, if > 1 pad an extra batch
-    max_number_of_chunks = n_frames / chunck_size_f
-    missing_n_frames = ceil(max_number_of_chunks) * chunck_size_f - n_frames
+    max_number_of_chunks = n_valid_frames / chunck_size_f
+    missing_n_frames = ceil(max_number_of_chunks) * chunck_size_f - n_valid_frames
     if missing_n_frames > 0:
         audio_t = torch.nn.functional.pad(
             audio_t, pad=(0, missing_n_frames), mode="constant", value=0
         )
-
-    # (batch, 32_000)
+    # (batch, 32_000) | batch_t[-1][-missing_n_frames] = first padded value
     batch_t = audio_t.reshape((ceil(max_number_of_chunks), chunck_size_f))
 
     # NOTE - pass batch_t through model
     # predicted output, to be processed
-    # whisperidou specific
-    if isinstance(model, Whisperidou):
-        batch_t = model.audio_preparation_hook(batch_t.cpu().numpy())["input_features"]
+    batch_t = model.audio_preparation_hook(batch_t.cpu().numpy())
 
     # (batch, windows, n_labels)
     output = model(batch_t)
@@ -57,11 +54,13 @@ def prediction(
     print(model.label_encoder.rev_map)
     all_intervals = Intervals()
     for batch_i, batch in enumerate(output):
+        # batch: (windows, n_labels)
         # Zip with prediction windows. or get them through computation (clip)
 
         # NOTE - for each window, create an interval per label
         # merge intervals by adding them to the Intervals structure
         for w_i, window in enumerate(batch):
+            # windows: (n_labels, )
             frame_start = batch_i * chunck_size_f + rf_start_i(
                 w_i, model.conv_settings.strides, model.conv_settings.paddings
             )
@@ -76,20 +75,32 @@ def prediction(
             for label in found_labels:
                 corresponding_interval: Interval = (frame_start, frame_end, label)
                 all_intervals.add(corresponding_interval)
-    #     if batch_i == 2:
-    #         print(all_intervals)
-    #         exit(55)
+
     # TODO remove x last prediction if audio was padded
     if missing_n_frames > 0:
-        raise NotImplementedError
+        for interval in all_intervals:
+            # remove from interlap / create new interlap object
+            if interval[0] > n_valid_frames - missing_n_frames:
+                pass
+                # raise NotImplementedError
 
-    # TODO generate aa & rttms
-    for start_f, end_f, label in all_intervals:
-        aa = AudioAnnotation(
-            uid=audio_path.stem,
-            start_time_ms=float(frames_to_milliseconds(start_f)),
-            duration_ms=float(frames_to_milliseconds(end_f - start_f)),
-            label=str(label),
-        )
+    # NOTE - generate & write; aa & rttms
+    rttm_out = output_p / "rttm"
+    aa_out = output_p / "aa"
+    rttm_out.mkdir(exist_ok=True, parents=True)
+    aa_out.mkdir(exist_ok=True, parents=True)
 
-        print(aa.to_rttm())
+    uri = audio_path.stem
+    with (
+        (rttm_out / uri).with_suffix(".rttm").open("w") as rttm_f,
+        (aa_out / uri).with_suffix(".aa").open("w") as aa_f,
+    ):
+        for start_f, end_f, label in all_intervals:
+            aa = AudioAnnotation(
+                uid=audio_path.stem,
+                start_time_ms=float(frames_to_milliseconds(start_f)),
+                duration_ms=float(frames_to_milliseconds(end_f - start_f)),
+                label=str(label),
+            )
+            rttm_f.write(aa.to_rttm() + "\n")
+            aa_f.write(aa.write() + "\n")

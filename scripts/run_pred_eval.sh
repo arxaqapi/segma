8866b65bef52330bee710c59102a1b0294e82e1d
arxaqapi <arxaqapi@protonmail.com>
2025-01-24 15:35:01 +0100

    feat: improved run_pred_eval.sh

--- scripts/run_pred_eval.sh
+++ scripts/run_pred_eval.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 #SBATCH --job-name=segma_pred_eval         # Job name
 #SBATCH --partition=gpu                    # Take a node from the 'gpu' partition
-## SBATCH --export=ALL                       # Export your environment to the compute node
+##SBATCH --export=ALL                       # Export your environment to the compute node
 #SBATCH --gres=gpu:1
 #SBATCH --mem=40G                         # ram
 #SBATCH --exclude=puck5                 # Do not run on puck5 node
 #SBATCH --cpus-per-task=4
-#SBATCH --time=5:00:00
+#SBATCH --time=20:00:00
 #SBATCH --output=logs/slurm-%j-pred-eval.out
 
 # srun python scripts/train.py --wandb --model pyannet --tags no_speech pyannet --dataset baby_train
@@ -18,15 +18,21 @@
 source .venv/bin/activate
 module load audio-tools
 
-srun python scripts/predict.py \
+model_id=20250122_234139-zgl0ahak
+ckpt="epoch=18-val_loss=2.132.ckpt"
+
+# model_id=20250123_003929-opn3jjm0
+# ckpt="epoch=04-val_loss=2.133.ckpt"
+
+uv run scripts/predict.py \
     --uris data/baby_train/test.txt \
     --wavs data/baby_train/wav \
-    --ckpt models/20241204_014516--duwjqyou/checkpoints/last.ckpt
-deactivate
+    --ckpt models/$model_id/checkpoints/$ckpt
+
 
 # 2. evaluate predictions
 source .venv_inference/bin/activate
 
-srun python scripts/evaluate.py \
+python scripts/evaluate.py \
     --gt data/baby_train/rttm \
-    --pred models/20241204_014516--duwjqyou/rttm
+    --pred models/$model_id/rttm
